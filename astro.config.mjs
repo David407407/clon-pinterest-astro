955897c0474bd168f29a09aa42f41c965b3b1734
David407407 <darksideinthespace@gmail.com>
2024-02-28 19:01:45 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import node from '@astrojs/node';
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  site: 'http://localhost:4321'
+  site: 'http://localhost:4321',
+  adapter: netlify()
 });
--- src/components/IdeasSinOrganizar.tsx
+++ src/components/IdeasSinOrganizar.tsx
@@ -11,7 +11,7 @@ const IdeasSinOrganizar = (props: Props) => {
     <div className='columns-2 md:columns-3 lg:columns-5 xl:columns-8 gap-4 w-10/12 mx-auto border-t-2 mt-28 pt-12'>
 			{
         favorites.map( (img: any) => (
-            <Pin key={Object.keys(img)[0]} id={Object.keys(img)[0]} imagen={img[Object.keys(img)[0]]} /> 
+            <Pin key={Object.keys(img)[0]} id={Object.keys(img)[0]} imagen={img[Object.keys(img)[0]]} abajo={false} /> 
         ))
       }
     </div>
